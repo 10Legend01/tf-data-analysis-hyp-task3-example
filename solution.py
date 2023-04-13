7a6d59c31639ce25fde5911f2e3be75d73a7e5aa
10Legend01 <mihailnazarov01@mail.ru>
2023-04-13 18:32:47 +0300

    do solution №1

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
+from statsmodels.stats.weightstats import ztest
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1379613676 # Ваш chat ID, не меняйте название переменной
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    return ztest(x, value=300.59252496635514, alternative='smaller')[1] < 0.1
+
+# historical_db = pd.read_csv("hyp3_historical_data.csv", index_col=0)
+
+# print(historical_db.iloc[0].mean())
