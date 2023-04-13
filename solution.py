959eb1ea4818d40d97ed69da3f7f5f833fbd663f
10Legend01 <mihailnazarov01@mail.ru>
2023-04-13 23:18:08 +0300

    do solution №2

--- solution.py
+++ solution.py
@@ -6,7 +6,7 @@ from statsmodels.stats.weightstats import ztest
 chat_id = 1379613676 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    return ztest(x, value=300.59252496635514, alternative='smaller')[1] < 0.1
+    return ztest(x, value=300, alternative='smaller')[1] < 0.1
 
 # historical_db = pd.read_csv("hyp3_historical_data.csv", index_col=0)
 
